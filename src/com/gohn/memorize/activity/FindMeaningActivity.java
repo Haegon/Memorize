8c5305f3208698eb62bcc3147e510b0192c0af50
HaegonKoh <gohn0929@gmail.com>
2015-01-01 20:54:32 +0900

    문제 푸는 액티비티 추가.

---
+++ src/com/gohn/memorize/activity/FindMeaningActivity.java
@@ -0,0 +1,14 @@
+package com.gohn.memorize.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FindMeaningActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
