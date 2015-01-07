f20154c7b9b1ff073cf5db377cd2eb3c7fc10a0a
HaegonKoh <gohn0929@gmail.com>
2015-01-08 00:20:30 +0900

    문제 랜덤 옵션 추가.

--- src/com/gohn/memorize/activity/ChoiceProblemActivity.java
+++ src/com/gohn/memorize/activity/ChoiceProblemActivity.java
@@ -1,13 +1,15 @@
 package com.gohn.memorize.activity;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -15,6 +17,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.gohn.memorize.R;
+import com.gohn.memorize.manager.Global;
 import com.gohn.memorize.manager.WordsDBMgr;
 import com.gohn.memorize.model.AnswerItem;
 import com.gohn.memorize.model.Exercise;
@@ -64,7 +67,10 @@ public class ChoiceProblemActivity extends Activity {
 			}
 
 		}
-				
+
+		if (Global.getInstance(getApplicationContext()).RandomProblem)
+			makeExercisesRandomly();
+
 		viewInit();
 		showPage();
 	}
@@ -198,6 +204,9 @@ public class ChoiceProblemActivity extends Activity {
 
 	public ArrayList<Exercise> assembleWrongExercises() {
 
+		if (Global.getInstance(getApplicationContext()).RandomProblem)
+			makeExercisesRandomly();
+
 		ArrayList<Exercise> ea = new ArrayList<Exercise>();
 
 		for (int i = 0; i < exercises.size(); i++) {
@@ -208,6 +217,11 @@ public class ChoiceProblemActivity extends Activity {
 			}
 		}
 		return ea;
+	}
+
+	public void makeExercisesRandomly() {
+		long seed = System.nanoTime();
+		Collections.shuffle(exercises, new Random(seed));
 	}
 
 	public boolean isUserCheck() {
--- src/com/gohn/memorize/activity/SettingsActivity.java
+++ src/com/gohn/memorize/activity/SettingsActivity.java
@@ -1,9 +1,15 @@
 package com.gohn.memorize.activity;
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.preference.SwitchPreference;
+import android.util.Log;
 
 import com.gohn.memorize.R;
+import com.gohn.memorize.manager.Global;
 
 public class SettingsActivity extends PreferenceActivity {
 
@@ -12,5 +18,39 @@ public class SettingsActivity extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 
 		addPreferencesFromResource(R.layout.preference_layout);
+
+		CheckBoxPreference checkRandom = (CheckBoxPreference) findPreference("randomProblem");
+		checkRandom.setOnPreferenceChangeListener(onPreferenceChangeListener);
 	}
+
+	private void setOnPreferenceChange(Preference mPreference) {
+		mPreference.setOnPreferenceChangeListener(onPreferenceChangeListener);
+
+		onPreferenceChangeListener.onPreferenceChange(mPreference, PreferenceManager.getDefaultSharedPreferences(mPreference.getContext()).getString(mPreference.getKey(), ""));
+	}
+
+	private Preference.OnPreferenceChangeListener onPreferenceChangeListener = new Preference.OnPreferenceChangeListener() {
+
+		@Override
+		public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+			if (preference instanceof CheckBoxPreference) {
+
+				if (preference.getKey().equals("randomProblem")) {
+					Global.getInstance(getApplicationContext()).RandomProblem = Boolean.valueOf(newValue.toString());
+				} else if (preference.getKey().equals("useUpdateNotify")) {
+
+				}
+			} else if (preference instanceof SwitchPreference) {
+
+				if (preference.getKey() == "autoUpdate ") {
+
+				}
+			}
+
+			return true;
+		}
+
+	};
+
 }
