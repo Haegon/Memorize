abf7f6eb64183b135cc97c5af526848cdf393812
gohn0929 <gohn0929@gmail.com>
2016-01-02 18:47:51 +0900

    app drawer 변경.

--- app/src/main/java/com/gohn/memorize/activity/MainActivity.java
+++ app/src/main/java/com/gohn/memorize/activity/MainActivity.java
@@ -2,18 +2,19 @@ package com.gohn.memorize.activity;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 
 import com.gohn.memorize.R;
 import com.gohn.memorize.common.CommonData;
@@ -23,6 +24,17 @@ import com.gohn.memorize.model.VocaGroup;
 import com.gohn.memorize.util.BackPressCloseHandler;
 import com.gohn.memorize.util.Dialog;
 import com.gohn.memorize.util.GLog;
+import com.gohn.memorize.util.Global;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.interfaces.OnCheckedChangeListener;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SwitchDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
 import java.util.ArrayList;
 
@@ -37,18 +49,21 @@ public class MainActivity extends AppCompatActivity
 
     CardArrayRecyclerViewAdapter mCardArrayAdapter;
     CardRecyclerView mRecyclerView;
-//    RecyclerView
+
+    private AccountHeader headerResult = null;
+    private Drawer result = null;
+    private boolean opened = false;
 
     BackPressCloseHandler backPressCloseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         DBMgr.init(this);
 
-        initView();
+        initView(savedInstanceState);
 
         mCardArrayAdapter = new CardArrayRecyclerViewAdapter(this, getCards());
 
@@ -66,7 +81,7 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    void initView() {
+    void initView(Bundle savedInstanceState) {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -78,15 +93,223 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.setDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // Create the AccountHeader
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.img_navi_title)
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+        //Create the drawer
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withHasStableIds(true)
+                .withAccountHeader(headerResult) //set the AccountHeader we created earlier for the header
+                .addDrawerItems(
+                        new PrimaryDrawerItem()
+                                .withName(R.string.navi_home)
+                                .withIcon(R.drawable.ic_home)
+                                .withIdentifier(R.string.navi_home)
+                                .withSelectable(false),
+                        new PrimaryDrawerItem()
+                                .withName(R.string.navi_help)
+                                .withIcon(R.drawable.ic_help)
+                                .withIdentifier(R.string.navi_help)
+                                .withSelectable(false),
+                        new PrimaryDrawerItem()
+                                .withName(R.string.navi_info)
+                                .withIcon(R.drawable.ic_info)
+                                .withIdentifier(R.string.navi_info)
+                                .withSelectable(false),
+                        new PrimaryDrawerItem()
+                                .withName(R.string.navi_donate)
+                                .withIcon(R.drawable.ic_money)
+                                .withIdentifier(R.string.navi_donate)
+                                .withSelectable(false),
+
+                        new DividerDrawerItem(),
+
+                        new SecondaryDrawerItem()
+                                .withName(R.string.navi_setting)
+                                .withIcon(R.drawable.ic_setting)
+                                .withIdentifier(R.string.navi_setting)
+                                .withSelectable(false),
+
+//                        new SwitchDrawerItem()
+//                                .withName(R.string.pref_basic_random)
+//                                .withDescription(R.string.pref_basic_random_des_on)
+//                                .withDescriptionTextColor(Color.GRAY)
+//                                .withIcon(R.drawable.ic_clip)
+//                                .withChecked(true)
+//                                .withIdentifier(R.string.pref_basic_random)
+//                                .withOnCheckedChangeListener(onCheckedChangeListener),
+
+                        new PrimaryDrawerItem()
+                                .withName(R.string.navi_share)
+                                .withIcon(R.drawable.ic_share)
+                                .withIdentifier(R.string.navi_share)
+                                .withSelectable(false),
+                        new PrimaryDrawerItem()
+                                .withName(R.string.navi_grade)
+                                .withIcon(R.drawable.ic_grade)
+                                .withIdentifier(R.string.navi_grade)
+                                .withSelectable(false),
+                        new PrimaryDrawerItem()
+                                .withName(R.string.navi_mail)
+                                .withIcon(R.drawable.ic_mail)
+                                .withIdentifier(R.string.navi_mail)
+                                .withSelectable(false)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem == null) return false;
+
+                        switch (drawerItem.getIdentifier()) {
+                            case R.string.navi_home:
+                                GLog.Debug("@@@@@ R.string.navi_home");
+                                break;
+                            case R.string.navi_help:
+                                GLog.Debug("@@@@@ R.string.navi_help");
+                                break;
+                            case R.string.navi_info:
+                                GLog.Debug("@@@@@ R.string.navi_info");
+                                break;
+                            case R.string.navi_donate:
+                                GLog.Debug("@@@@@ R.string.navi_donate");
+                                break;
+                            case R.string.navi_setting:
+                                GLog.Debug("@@@@@ R.string.navi_setting");
+                                if (opened) {
+                                    //remove the items which are hidden
+                                    result.removeItems(R.string.pref_basic_random);
+                                } else {
+                                    int curPos = result.getPosition(drawerItem);
+                                    result.addItemsAtPosition(
+                                            curPos,
+                                            new SwitchDrawerItem()
+                                                    .withName(R.string.pref_basic_random)
+                                                    .withDescription(R.string.pref_basic_random_des_on)
+                                                    .withDescriptionTextColor(Color.GRAY)
+                                                    .withLevel(2)
+                                                    .withIcon(R.drawable.ic_clip)
+                                                    .withChecked(Global.getBoolean(MainActivity.this,CommonData.GLOBAL_KEY_RANDOM,false))
+                                                    .withIdentifier(R.string.pref_basic_random)
+                                                    .withOnCheckedChangeListener(onCheckedChangeListener)
+                                                    .withSelectable(false)
+                                    );
+                                }
+                                opened = !opened;
+                                return true;
+                            case R.string.pref_basic_random:
+                                GLog.Debug("@@@@@ R.string.pref_basic_random");
+                                break;
+                            case R.string.navi_share:
+                                GLog.Debug("@@@@@ R.string.navi_share");
+                                break;
+                            case R.string.navi_grade:
+                                GLog.Debug("@@@@@ R.string.navi_grade");
+                                break;
+                            case R.string.navi_mail:
+                                GLog.Debug("@@@@@ R.string.navi_mail");
+                                break;
+                        }
+
+//                        if (drawerItem != null) {
+//                            Intent intent = null;
+//                            if (drawerItem.getIdentifier() == 1) {
+//                                intent = new Intent(DrawerActivity.this, CompactHeaderDrawerActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 2) {
+//                                intent = new Intent(DrawerActivity.this, ActionBarActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 3) {
+//                                intent = new Intent(DrawerActivity.this, MultiDrawerActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 4) {
+//                                intent = new Intent(DrawerActivity.this, NonTranslucentDrawerActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 5) {
+//                                intent = new Intent(DrawerActivity.this, AdvancedActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 6) {
+//                                intent = new Intent(DrawerActivity.this, KeyboardUtilActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 7) {
+//                                intent = new Intent(DrawerActivity.this, EmbeddedDrawerActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 8) {
+//                                intent = new Intent(DrawerActivity.this, FullscreenDrawerActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 9) {
+//                                intent = new Intent(DrawerActivity.this, CustomContainerActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 10) {
+//                                intent = new Intent(DrawerActivity.this, MenuDrawerActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 11) {
+//                                intent = new Intent(DrawerActivity.this, MiniDrawerActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 12) {
+//                                intent = new Intent(DrawerActivity.this, FragmentActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 13) {
+//                                intent = new Intent(DrawerActivity.this, CollapsingToolbarActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 14) {
+//                                intent = new Intent(DrawerActivity.this, PersistentDrawerActivity.class);
+//                            } else if (drawerItem.getIdentifier() == 15) {
+//                                intent = new Intent(DrawerActivity.this, CrossfadeDrawerLayoutActvitiy.class);
+//                            } else if (drawerItem.getIdentifier() == 19) {
+//                                //showcase a simple collapsable functionality
+//                                if (opened) {
+//                                    //remove the items which are hidden
+//                                    result.removeItems(2000, 2001);
+//                                } else {
+//                                    int curPos = result.getPosition(drawerItem);
+//                                    result.addItemsAtPosition(
+//                                            curPos,
+//                                            new SecondaryDrawerItem().withName("CollapsableItem").withLevel(2).withIcon(GoogleMaterial.Icon.gmd_8tracks).withIdentifier(2000),
+//                                            new SecondaryDrawerItem().withName("CollapsableItem 2").withLevel(2).withIcon(GoogleMaterial.Icon.gmd_8tracks).withIdentifier(2001)
+//                                    );
+//                                }
+//                                opened = !opened;
+//                                return true;
+//                            } else if (drawerItem.getIdentifier() == 20) {
+//                                intent = new LibsBuilder()
+//                                        .withFields(R.string.class.getFields())
+//                                        .withActivityStyle(Libs.ActivityStyle.LIGHT_DARK_TOOLBAR)
+//                                        .intent(DrawerActivity.this);
+//                            }
+//                            if (intent != null) {
+//                                DrawerActivity.this.startActivity(intent);
+//                            }
+//                        }
+
+                        return false;
+                    }
+                })
+//                .withSavedInstance(savedInstanceState)
+                .withShowDrawerOnFirstLaunch(true)
+                .build();
+
+        //only set the active selection or active profile if we do not recreate the activity
+        if (savedInstanceState == null) {
+            // set the selection to the item with the identifier 11
+            result.setSelection(R.string.navi_home, false);
+        }
     }
+
+    private OnCheckedChangeListener onCheckedChangeListener = new OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(IDrawerItem drawerItem, CompoundButton buttonView, boolean isChecked) {
+
+            if ( drawerItem == null ) return;
+
+            switch (drawerItem.getIdentifier()) {
+                case R.string.pref_basic_random:
+                    Global.setBoolean(MainActivity.this,CommonData.GLOBAL_KEY_RANDOM,isChecked);
+                    break;
+            }
+        }
+    };
 
     ArrayList<Card> getCards() {
         ArrayList<VocaGroup> vocaGroups = DBMgr.getInstance().getVocaGroups();
@@ -143,13 +366,20 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        //add the values which need to be saved from the drawer to the bundle
+        outState = result.saveInstanceState(outState);
+        //add the values which need to be saved from the accountHeader to the bundle
+        outState = headerResult.saveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (result != null && result.isDrawerOpen()) {
+            result.closeDrawer();
         } else {
 //            super.onBackPressed();
-
             backPressCloseHandler.onBackPressed();
         }
     }
