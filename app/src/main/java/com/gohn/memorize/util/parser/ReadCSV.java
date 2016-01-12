7e0a6a70014d0b5ff322a43f37e26da958929e81
HaegonKoh <gohn0929@gmail.com>
2016-01-12 18:48:15 +0900

    csv 라이브러리 추가.

--- app/src/main/java/com/gohn/memorize/util/parser/ReadCSV.java
+++ app/src/main/java/com/gohn/memorize/util/parser/ReadCSV.java
@@ -1,8 +1,13 @@
 package com.gohn.memorize.util.parser;
 
 import com.gohn.memorize.model.WordSet;
+import com.gohn.memorize.model.WordType;
+import com.opencsv.CSVReader;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ReadCSV {
@@ -13,35 +18,31 @@ public class ReadCSV {
 
 		ArrayList<WordSet> words = new ArrayList<WordSet>();
 
-//		try {
-//			CSVReader reader = new CSVReader(new FileReader(inputWorkbook));
-//
-//			try {
-//
-//				for (;;) {
-//					String[] row= reader.readNext();
-//
-//					if (row == null)
-//						break;
-//
-//					WordSet word = new WordSet("",row[0],row[1],row[2]);
-//
-////					word.Type = row[0];
-////					word.Word = row[1];
-////					word.Meaning = row[2];
-//
-//					if (!WordType.isType(word.getType()))
-//						word.setType(WordType.ETC);
-//					words.add(word);
-//				}
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		}
+		try {
+			CSVReader reader = new CSVReader(new FileReader(inputWorkbook));
+
+			try {
+
+				for (;;) {
+					String[] row= reader.readNext();
+
+					if (row == null)
+						break;
+
+					WordSet word = new WordSet("",row[0],row[1],row[2]);
+
+					if (!WordType.isType(word.getType()))
+						word.setType(WordType.ETC);
+					words.add(word);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 
 		return words;
 	}
